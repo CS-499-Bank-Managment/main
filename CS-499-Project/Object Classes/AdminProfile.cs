850ea22f9fd22ab5d1b4bb20b0ac4ad9ffd3f126
Jennifer <jlb0092@uah.edu>
2018-09-14 15:03:21 -0500

    Adding initial classes for all profile types

---
+++ CS-499-Project/Object Classes/AdminProfile.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CS_499_Project.Object_Classes
+{
+    public class AdminProfile : ProfileInterface
+{
+
+        public AdminProfile()
+        {
+            this.profile_type = ProfileType.ADMIN;
+        }
+}
+}
---
+++ CS-499-Project/Object Classes/TellerProfile.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CS_499_Project.Object_Classes
+{
+    public class TellerProfile
+{
+        public TellerProfile()
+        {
+
+        }
+}
+}
