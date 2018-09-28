e62dc9215ced48f6cb146dd368ebc9b48796e94a
claythearc <clay-turner@hotmail.com>
2018-09-28 16:42:07 -0500

    MOngo DB Support

--- CS-499-Project/Controllers/AdminController.cs
+++ CS-499-Project/Controllers/AdminController.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 using CS_499_Project.Object_Classes;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using MongoDB;
+using MongoDB.Driver;
+using MongoDB.Bson;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -37,6 +40,22 @@ namespace CS_499_Project.Controllers
             ViewBag.status = foo.Check(username);
                 
             
+            return View();
+        }
+
+        public IActionResult Mongo()
+        {
+            
+            var client = new MongoClient("Hidden Connection String.");
+            var database = client.GetDatabase("test");
+            var info = database.GetCollection<BsonDocument>("test");
+            var bstr = new BsonDocument
+            {
+                {"username", "clay"}
+            };
+
+            ViewBag.db = info.Find(bstr).ToList() ;
+
             return View();
         }
     }
