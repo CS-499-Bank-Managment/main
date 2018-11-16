1612c91d0cbec2b060f2edcb058f7130e92e4e0a
claythearc <clay-turner@hotmail.com>
2018-11-16 13:35:37 -0600

    latest db

---
+++ CS-499-Project/Object Classes/InterestHandler.cs
@@ -0,0 +1,14 @@
+using System.Timers;
+using System.Threading;
+
+namespace CS_499_Project.Object_Classes
+{
+    public class InterestHandler
+    {
+        private System.Timers.Timer timers;
+        public InterestHandler()
+        {
+            
+        }
+    }
+}
